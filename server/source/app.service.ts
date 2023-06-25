b595cb39e66b5bf264c1fa03b552ea91c9b69e75
章生 <me@jiluo.cc>
2023-06-25 21:53:53 +0800

    docs: task

--- server/source/app.service.ts
+++ server/source/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      type: 'qiniuyun',
+    };
+    return 'Hello World! 333';
   }
 }
