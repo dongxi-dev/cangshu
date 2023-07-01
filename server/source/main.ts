b51b304069a22c616e7378402c8945fa005a705b
章生 <me@jiluo.cc>
2023-07-01 11:22:31 +0800

    fix: import fail

--- server/source/main.ts
+++ server/source/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { PrismaService } from 'nestjs-prisma';
+import { DBService } from '@j.l/nestjs-database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService);
+  const prismaService = app.get(DBService);
   await prismaService.enableShutdownHooks(app);
 
   const options = new DocumentBuilder()
