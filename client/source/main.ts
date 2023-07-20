bbe0a455e13785ea604308b71c73b42a6ebbe19e
章生 <me@jiluo.cc>
2023-07-20 21:50:36 +0800

    feat: import prettier

--- client/source/main.ts
+++ client/source/main.ts
@@ -1,40 +1,40 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import ElementPlus, { ElMessage } from "element-plus";
-import client from "@j-l/request";
-import "element-plus/dist/index.css";
-import DefaultLayout from "~/layouts/default.vue";
-import routes from "~/routes";
-import { API_BASE_URL, APP_ID } from "~/constants";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus, { ElMessage } from 'element-plus'
+import client from '@j-l/request'
+import 'element-plus/dist/index.css'
+import DefaultLayout from '~/layouts/default.vue'
+import routes from '~/routes'
+import { API_BASE_URL, APP_ID } from '~/constants'
 
 client.config({
   base: API_BASE_URL,
-  contentType: "json",
-  responseType: "json",
+  contentType: 'json',
+  responseType: 'json',
   withCredentials: true,
   onResponse(response) {
     if (response.status === 401) {
-      ElMessage("未登录");
+      ElMessage('未登录')
       setTimeout(() => {
-        routes.push("/login");
-      }, 300);
+        routes.push('/login')
+      }, 300)
     }
     if (response.status > 399) {
-      ElMessage("失败");
-      throw new Error("请求异常");
+      ElMessage('失败')
+      throw new Error('请求异常')
     }
-    return response.body;
+    return response.body
   },
-});
+})
 
 const app = createApp(DefaultLayout)
   .use(createPinia())
   .use(routes)
   .use(ElementPlus)
-  .mount("#app");
+  .mount('#app')
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
+  app.component(key, component)
 }
-console.log(APP_ID);
+console.log(APP_ID)
