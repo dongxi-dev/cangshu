1c5d9590c91423d8c272957d939fd6d0f6260b18
章生 <me@jiluo.cc>
2023-07-26 23:09:02 +0800

    feat: OSAdapter

--- server/source/file-transfer/file-transfer.service.ts
+++ server/source/file-transfer/file-transfer.service.ts
@@ -4,6 +4,41 @@ import { ACCESS_KEY, BUCKET_NAME, SECRET_KEY } from '../constants';
 
 const mac = new auth.digest.Mac(ACCESS_KEY, SECRET_KEY);
 
+interface OSAdapter {
+  new (options: {
+    accessId: string;
+    accessKey: string;
+    region: string;
+    bucket: string;
+    basePath: string;
+  }): any;
+  accessId: string;
+  getToken(): string | Promise<string>;
+  getURL(key: string): string | Promise<string>;
+}
+
+class TencentOSAdapter implements OSAdapter {
+  accessId: string;
+
+  constructor(options: {
+    accessId: string;
+    accessKey: string;
+    region: string;
+    bucket: string;
+    basePath: string;
+  }) {
+    this.accessId = options.accessId;
+  }
+
+  getToken(): string | Promise<string> {
+    return '';
+  }
+
+  getURL(key: string): string | Promise<string> {
+    return '';
+  }
+}
+
 @Injectable()
 export class FileTransferService {
   getToken() {
